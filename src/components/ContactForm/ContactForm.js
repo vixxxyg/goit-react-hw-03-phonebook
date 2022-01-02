70d019ad4d512c17e5fe56921dfc38aa3f2c739b
Victoriya Gilenko <victoriyagilenko@gmail.com>
2022-01-02 16:59:22 +0100

    2

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import s from './ContactForm.module.css';
+import shortid from 'shortid';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = shortid.generate();
+  numberInputId = shortid.generate();
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form className={s.form} onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameInputId} className={s.label}>
+          Name:
+          <input
+            className={s.fieldInput}
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            id={this.nameInputId}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label htmlFor={this.numberInputId} className={s.label}>
+          Number:
+          <input
+            className={s.fieldInput}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+            placeholder="+380 (12) 345-67-89"
+            id={this.numberInputId}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
